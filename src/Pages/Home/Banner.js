7b93c3a47409922cd6d855b58df136f844de03a5
ahanafislam <ahanafislam@gmail.com>
2022-05-24 05:53:15 +0600

    Make footer

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                     heroText="We can make the best quality industrial pressure washers, automatic screwdrivers, circular saws, rotary hammers, drill machines, impact wrenches, chainsaws and many more."
                 />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn border-0 bg-inherit btn-primary">❮</a> 
+                    <a href="#slide3" className="btn border-0 bg-inherit btn-primary">❮</a> 
                     <a href="#slide2" className="btn border-0 bg-inherit btn-primary">❯</a>
                 </div>
             </div> 
