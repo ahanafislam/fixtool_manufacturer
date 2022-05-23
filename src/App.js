5f7639e1b31f64729d4f5b2bf6d3ddf3bab35bcc
ahanafislam <ahanafislam@gmail.com>
2022-05-24 00:04:23 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar bg-primary">
+        <a className="btn btn-ghost normal-case text-3xl text-white">Fix<span className='text-secondary'>Tool</span></a>
+      </div>
     </div>
   );
 }
