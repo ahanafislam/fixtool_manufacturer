20e7214921960aff53eb9e12d9af0bea8417731a
ahanafislam <ahanafislam@gmail.com>
2022-05-26 07:09:32 +0600

    Create google login system

--- src/Pages/Login/SocialLogin.js
+++ src/Pages/Login/SocialLogin.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import { FcGoogle } from 'react-icons/fc';
+import Loading from '../Shared/Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    useEffect(() => {
+        googleUser && console.log("you are login");
+    },[googleUser]);
+
+    if(googleLoading){
+        return <Loading></Loading>
+    }
+
     return (
     <>  
         <div className="flex flex-col w-full px-4 border-opacity-50">
             <div className="divider text-primary">OR</div>
         </div>
         <div className="card-actions justify-center mb-4">
-            <button className="btn text-white btn-outline btn-primary">
+            <button onClick={() => signInWithGoogle()} className="btn text-white btn-outline btn-primary">
                 <FcGoogle className='text-3xl mr-2'/> Continue With Google
             </button>
         </div>
